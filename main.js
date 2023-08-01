37cdc66e518a42077a41e0a1c072e241309d926b
Elizin1 <113482607+Elizin1@users.noreply.github.com>
2023-08-01 20:44:26 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 song = "";
-scoreLeftWrist = "";
 
-rightWristX = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+
 rightWristY = 0;
+rightWristX = 0;
 
 leftWristX = 0;
 leftWristY = 0;
@@ -29,7 +31,8 @@ function gotPoses(results){
     if(results.length > 0){
         console.log(results);
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist - " + scoreLeftWrist);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist - " + scoreLeftWrist + "scoreLeftWrist = " + scoreLeftWrist);
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWrist X = " + rightWristX + " rightWrist Y = " + rightWristY);
@@ -44,14 +47,41 @@ function draw(){
     fill("#FF0000");
     stroke("#FF0000");
 
-    if(scoreLeftWrist > 0.2){
-        circle(leftWristX, leftWristY, 20);
-        inNumberleftWristY = Number(leftWristY);
-        remove_decimals = floor(inNumberleftWristY);
+    if(scoreRightWrist > 0.2){
+        circle(rightWristX, rightWristY, 20);
+
+        if (rightWristY > 0 && rightWristY <= 100){
+            document.getElementById("speed").innerHTML = "velocidade = 0.5x";
+            song.rate(0.5);
+        }
+
+        else if(rightWristY > 100 && rightWristY <= 200){
+            document.getElementById("speed").innerHTML = "velocidade = 1x";
+            song.rate(1);
+        }
+
+        else if(rightWristY >200 && rightWristY <= 300){
+            document.getElementById("speed").innerHTML = velocidade = "velocidade = 1.5x";
+            song.rate(1.5);
+        }
+
+        else if(rightWristY > 300 && rightWristY <= 400){
+            document.getElementById("speed").innerHTML = "velocidade = 2x";
+            song.rate(2);
+        }
+
+        else if(rightWristY > 400){
+            document.getElementById("speed").innerHTML = "velocidade = 2.5x";
+            song.rate(2.5);
+        }
+    }
+        if (scoreLeftWrist > 0.2){
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
         volume = remove_decimals/500;
         document.getElementById("volume").innerHTML = "volume = " + volume;
         song.setVolume(volume);
-    }
+        } 
 }
 
 function play(){
